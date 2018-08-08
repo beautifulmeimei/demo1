bd820a8d0cb23816103a57332baf5eab1716727c
beautifulbanana <18943107757@163.com>
2018-08-08 21:32:13 +0800

    mango01分支2次

--- lucene/src/main/java/com/mango/domain/User.java
+++ lucene/src/main/java/com/mango/domain/User.java
@@ -3,6 +3,15 @@ package com.mango.domain;
 public class User {
 
 	private String name;
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
